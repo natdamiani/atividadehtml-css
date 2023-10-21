9f3f2b0f53c9fae38b3e6076e0715f080efbc28c
natdamiani <148651118+natdamiani@users.noreply.github.com>
2023-10-21 16:01:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+var imagens1 = ["imagem1-1.svg", "imagem1-2.svg", "imagem1-3.svg"];
+var imagemAtual1 = 0;
+
+function trocarImagem1() {
+    imagemAtual1 = (imagemAtual1 + 1) % imagens1.length;
+    document.getElementById("figura1").src = imagens1[imagemAtual1];
+}
+
+
+var imagens2 = ["imagem2-1.svg", "imagem2-2.svg", "imagem2-3.svg"];
+var imagemAtual2 = 0;
+
+function trocarImagem2() {
+    imagemAtual2 = (imagemAtual2 + 1) % imagens2.length;
+    document.getElementById("figura2").src = imagens2[imagemAtual2];
+}
+
+
+var imagens3 = ["imagem3-1.svg", "imagem3-2.svg", "imagem3-3.svg"];
+var imagemAtual3 = 0;
+
+function trocarImagem3() {
+    imagemAtual3 = (imagemAtual3 + 1) % imagens3.length;
+    document.getElementById("figura3").src = imagens3[imagemAtual3];
+}
+
+
+var imagens4 = ["imagem4-1.svg", "imagem4-2.svg", "imagem4-3.svg"];
+var imagemAtual4 = 0;
+
+function trocarImagem4() {
+    imagemAtual4 = (imagemAtual4 + 1) % imagens4.length;
+    document.getElementById("figura4").src = imagens4[imagemAtual4];
+}
+
+
+var imagens5 = ["imagem5-1.svg", "imagem5-2.svg", "imagem5-3.svg"];
+var imagemAtual5 = 0;
+
+function trocarImagem5() {
+     imagemAtual5 = (imagemAtual5 + 1) % imagens5.length;
+    document.getElementById("figura5").src = imagens5[imagemAtual5];
+}
+
+var imagens6 = ["imagem6-1.svg", "imagem6-2.svg", "imagem6-3.svg"];
+var imagemAtual6 = 0;
+
+function trocarImagem6() {
+    imagemAtual6 = (imagemAtual6 + 1) % imagens6.length;
+    document.getElementById("figura6").src = imagens6[imagemAtual6];
+}
+
+
+var imagens7 = ["imagem7-1.svg", "imagem7-2.svg", "imagem7-3.svg"];
+var imagemAtual7 = 0;
+
+function trocarImagem7() {
+    imagemAtual7 = (imagemAtual7 + 1) % imagens7.length;
+    document.getElementById("figura7").src = imagens7[imagemAtual7];
+}
+
+
+var imagens8 = ["imagem8-1.svg", "imagem8-2.svg", "imagem8-3.svg"];
+var imagemAtual8 = 0;
+
+function trocarImagem8() {
+    imagemAtual8 = (imagemAtual8 + 1) % imagens8.length;
+    document.getElementById("figura8").src = imagens8[imagemAtual8];
+}
+
+
+var imagens9 = ["imagem9-1.svg", "imagem9-2.svg", "imagem9-3.svg"];
+var imagemAtual9 = 0;
+
+function trocarImagem9() {
+    imagemAtual9 = (imagemAtual9 + 1) % imagens9.length;
+    document.getElementById("figura9").src = imagens9[imagemAtual9];
+}
+
+
+var imagens10 = ["imagem10-1.svg", "imagem10-2.svg", "imagem10-3.svg"];
+var imagemAtual10 = 0;
+
+function trocarImagem10() {
+    imagemAtual10 = (imagemAtual10 + 1) % imagens10.length;
+    document.getElementById("figura10").src = imagens10[imagemAtual10];
+}
